7bc57301630eb05ec402c43de4c71cf081c33a93
Dawit Tirore <dawit.tirore@gmail.com>
2026-07-25 12:58:16 +0200

    add documentation to models.v

--- theories/models.v
+++ theories/models.v
@@ -18,7 +18,10 @@ Open Scope order_scope.
 
 Require Export NonInterference.theories.theorems.
 
-(*In this file we define models for process scheduling in the presence of timer and disk interrupts*)
+(* This file defines the process-scheduling models studied in the paper: three
+   processes over interrupt inputs, culminating in [wrapped_model_good], the
+   model proved non-interfering in noninterference.v.  The type abbreviations
+   below ([T_in], [T_out'], [T_out]) fix the input/output interfaces. *)
 
 Fixpoint times_n n (f : nat -> Ty) : Ty :=
   let t := f n in
@@ -33,40 +36,58 @@ Definition T_in := TInterrupt.
 Definition T_out' := Times (Option TPublicOutput) (Times (Option TTypeSyscall) (Times (Option Nat) (times_on 2 (fun _ => THandlerOutput)))).
 Definition T_out := Option (Sum TPublicOutput TTypeSyscall).
 
-(* ================================================================= *)
-(*We define 3 models in this file:
+(* =================================================================
+   The three models (real Coq names, defined in sections 7-9 below):
 
- model1: Proc T_in T_out'
- Normal handling of interrupts, meaning that interrupts that an attacker should not know about will be leaked through scheduling
+     model_bad          : Proc T_in T_out'
+       Normal interrupt handling: interrupts an attacker should not
+       know about leak through scheduling.
 
- model2: Proc T_in T_out'
- The masking of interrupts is controlled so that secret interrupts can be received without causing information leaks.
- All output of the process pool is shown here, from user space, scheduler and all handlers.
+     model_good         : Proc T_in T_out'
+       Interrupt masking is controlled so that secret interrupts can
+       be received without leaking.  Shows every process-pool output
+       (user space, scheduler and all handlers).
 
- model3: Proc T_in T_out
- The final model, defined as a wrapper around model2 showing only outputs of user space processes*)
- 
-(* ================================================================= *)
+     wrapped_model_good : Proc T_in T_out
+       The final model: a wrapper around model_good exposing only the
+       user-space outputs.
 
-(*Structure:
-  1) Process definitions: public and private userspace processes, scheduler, handler
-  2) Process pool: A generic process definition that builds a pool from an indexed set of processes
-  3) Stateful wrapper for process pool
-  4) Defining state for model (interrupt controller, active process id, etc...)
-  5) Generic state transition function
-  6) Instantiate definitions to obtain:
-    a) model1
-    b) model2
-    c) model3
- *)
+   Contents:
+     1. Userspace processes, scheduler and interrupt handler
+     2. Process pool
+     3. Stateful wrapper
+     4. Model state
+     5. State transitions
+     6. Trace vocabulary
+     7. model_bad
+     8. model_good
+     9. wrapped_model_good
+   ================================================================= *)
+
+(* A note on notation.  Processes have type [Proc I O] where I and O range over
+   the inductive [Ty] of interface types, and [ [t] ] denotes the Coq type that
+   [t : Ty] encodes.  Indexing by [Ty] rather than by Coq's [Set] is what makes
+   reductions invertible in the proofs, but it also forces the explicit [@] and
+   [Ty] annotations below.  To keep the constructions readable, each process
+   definition is preceded by a comment giving the same term with those
+   annotations erased. *)
 
 
+(* === 1. Userspace processes, scheduler and interrupt handler === *)
+
+(* low_p = out GetRequest *)
 
 Definition low_p := @out Unit TPublicOutput GetRequest.
 
 
+(* alternate x y z pred =
+     map inl (fun o => if o is inr (true, _) then x else y)
+       (loop (map id inr
+         (sta (fun i v => if i is inl i' then v || pred i' else false)
+              (fun o v => v) false
+              (out z)))) *)
 Definition alternate (A B C : Ty) (x y : [B]) (z : [C]) (pred : [A] -> bool) :=
-  @map _ (Sum _ _) (Sum _ (Times Bool C)) B 
+  @map _ (Sum _ _) (Sum _ (Times Bool C)) B
                         inl
                         (fun o => if o is inr (true,z) then x else y)
                         (@loop (Sum A (Times Bool C))
@@ -77,27 +98,32 @@ Definition alternate (A B C : Ty) (x y : [B]) (z : [C]) (pred : [A] -> bool) :=
                            false
                            (@out (Times Bool _ ) C z)
                         )))).
+(* high_p = alternate Syscall NOP tt (fun i => i == Notify) *)
 Definition high_p := @alternate THandlerOutput TTypeSyscall Unit Syscall NOP tt (fun i => i == Notify).
 
-Definition scheduler : Proc Empty Nat.
-  eapply map. exact id.
-  instantiate (1:= Times Nat Unit). exact (fun o => fst o + 2). (*skip first three processes to reach high_p (0) and low_p (1) *)
-  eapply sta. exact (fun _ v => v). exact (fun _ v => v.+1%%2).
-  exact 1. (*Will start scheduling high process*)
-  apply out. exact tt.
-Defined.
+(* scheduler =
+     map id (fun o => o.1 + 2)
+       (sta (fun _ v => v) (fun _ v => v.+1 %% 2) 1 (out tt)) *)
+Definition scheduler : Proc Empty Nat :=
+  map (O := Times Nat Unit) (O' := Nat) id
+    (fun o => o.1 + 2)                        (*skip the interrupt handlers to reach high_p and low_p*)
+    (sta (V := Nat) (O := Unit)
+       (fun _ v => v) (fun _ v => v.+1 %% 2)
+       1                                      (*will start scheduling the high process*)
+       (out (O := Unit) tt)).
 
 Definition handler_type := Proc Unit THandlerOutput.
 
-Definition I_handler : handler_type.
-  rewrite /handler_type.
-  eapply map. exact id.
-  instantiate (1:= Times Nat Unit).
-  exact (fun o => if o.1 == 0 then Notify else Nothing).
-  eapply sta. exact (fun _ v => v). exact (fun _ v => v.+1%%2).
-  exact 0.
-  apply out. exact tt.
-Defined.
+(* I_handler =
+     map id (fun o => if o.1 == 0 then Notify else Nothing)
+       (sta (fun _ v => v) (fun _ v => v.+1 %% 2) 0 (out tt)) *)
+Definition I_handler : handler_type :=
+  map (O := Times Nat Unit) (O' := THandlerOutput) id
+    (fun o => if o.1 == 0 then Notify else Nothing)
+    (sta (V := Nat) (O := Unit)
+       (fun _ v => v) (fun _ v => v.+1 %% 2)
+       0
+       (out (O := Unit) tt)).
 
 Definition my_f_I (n : nat) :=
   match n with
@@ -121,61 +147,61 @@ Definition my_f_O (n : nat) :=
   | _ => Unit (*padding*)           
   end.
 
-Definition unit_proc : Proc Unit Unit.
-  apply out. exact tt.
-Defined.
+(* unit_proc = out tt *)
+Definition unit_proc : Proc Unit Unit := out (O := Unit) tt.
 
-(*Indexed processes*)
-Definition my_procs : forall n, Proc (my_f_I n) (my_f_O n).
-  case. apply I_handler. (*timer interrupt handler*)
-  case. apply I_handler. (*disk interrupt handler*)
-  case. apply I_handler. (*default handler*)
-  case. cbv. apply scheduler.
-  case. apply high_p.  
-  case. apply low_p.
-  elim. cbv. apply unit_proc.
-  intros. apply unit_proc.
-Defined.
+(* -- Indexed family of processes -- *)
+Definition my_procs : forall n, Proc (my_f_I n) (my_f_O n) :=
+  fun n => match n with
+           | 0        (*timer interrupt handler*)
+           | 1        (*disk interrupt handler*)
+           | 2        (*default handler*)
+             => I_handler
+           | 3 => scheduler
+           | 4 => high_p
+           | 5 => low_p
+           | _ => unit_proc (*padding*)
+           end.
 
 
-(* 2) Process pool *)
-Definition process_pool
-  (cur_pid : Ty)           
+(* === 2. Process pool === *)
+
+(* process_pool n f_coopt f_initial f_I f_O f_proj f_pid f_proc =
+     match n with
+     | 0 =>
+         map (fun i => (f_pid i.1 == 0, f_proj i.2 0)) id
+           (swi (f_initial 0) (maybe (map id (fun o => (f_coopt 0, o)) (f_proc 0))))
+     | n0.+1 =>
+         par (map (fun i => (f_pid i.1 == n0.+1, f_proj i.2 n0.+1)) id
+                (swi (f_initial n0.+1) (maybe (map id (fun o => (f_coopt n0.+1, o)) (f_proc n0.+1)))))
+             (process_pool n0 f_coopt f_initial f_I f_O f_proj f_pid f_proc)
+     end *)
+Fixpoint process_pool
+  (cur_pid : Ty)
   (n : nat)
   (f_coopt : nat -> bool)
   (f_initial : nat -> bool)
-  (f_I f_O : nat -> Ty)  
+  (f_I f_O : nat -> Ty)
   (T' : Ty)
   (f_proj : [T'] -> forall n, [(Option (f_I n))])
   (f_pid : [cur_pid] -> [Nat])
-  (f_proc : forall n, Proc (f_I n) (f_O n)) : Proc (Times cur_pid T') (times_on n f_O).
-  elim: n.
-  - simpl.
-    eapply map. simpl.
-    instantiate (1:= Times Bool (Option (f_I 0))). exact (fun i => (f_pid i.1 == 0, f_proj i.2 0)). 
-    exact id.  
-    apply swi. exact (f_initial 0). 
-    apply maybe. 
-    eapply map.
-      eapply id. 
-      exact (fun o => (f_coopt 0,o)).
-    exact (f_proc 0).
-
-  - intros. rewrite /times_on. simpl.
-    apply par.
-    * eapply map.
-      instantiate (1:= Times Bool (Option (f_I n.+1))). simpl.
-      exact (fun i => (f_pid i.1 == n.+1,f_proj i.2 n.+1)).
-        exact id. 
-      eapply swi.
-        exact (f_initial n.+1). 
-      eapply maybe.
-      eapply map.
-        exact id.     
-        exact (fun o => (f_coopt n.+1,o)).
-        exact (f_proc n.+1).
-        apply H.
-Defined.
+  (f_proc : forall n, Proc (f_I n) (f_O n)) {struct n}
+  : Proc (Times cur_pid T') (times_on n f_O) :=
+  (* process n: gate the input by [f_pid = n], run [f_proc n] behind a switch, and
+     par it with the pool for the remaining processes. *)
+  match n as n1 return Proc (Times cur_pid T') (times_on n1 f_O) with
+  | 0 =>
+      map ((fun i => (f_pid i.1 == 0, f_proj i.2 0))
+             : [Times cur_pid T'] -> [Times Bool (Option (f_I 0))]) id
+          (swi (f_initial 0)
+             (maybe (map id ((fun o => (f_coopt 0, o)) : [f_O 0] -> [Times Bool (f_O 0)]) (f_proc 0))))
+  | n0.+1 =>
+      par (map ((fun i => (f_pid i.1 == n0.+1, f_proj i.2 n0.+1))
+                  : [Times cur_pid T'] -> [Times Bool (Option (f_I n0.+1))]) id
+               (swi (f_initial n0.+1)
+                  (maybe (map id ((fun o => (f_coopt n0.+1, o)) : [f_O n0.+1] -> [Times Bool (f_O n0.+1)]) (f_proc n0.+1)))))
+          (@process_pool cur_pid n0 f_coopt f_initial f_I f_O T' f_proj f_pid f_proc)
+  end.
 
 Definition cur_pid := Sum Bool Nat.
 Definition initial_pid : [cur_pid] := inr 3. (*starting with low process*)
@@ -196,30 +222,26 @@ Definition my_f_pid (pid : [cur_pid]) : [Nat] :=
 Definition my_f_initial (n : nat) := n == (my_f_pid initial_pid).
 
 Definition T_intermediate := Option THandlerOutput.
-Definition f_proj (i : [T_intermediate]) : forall n, [Option (my_f_I n)].
-  simpl. rewrite /my_f_I. intros.
-  case: n. exact None. (*timer interrupt handler does not need any information*)
-  case. exact None.
-  case. exact None.
-  case. exact None. (*scheduler receives nothing*)
-  case. exact i. (*high process receives dI outut*)
-  case. exact None. (*low process receives nothing*)
-  move=>_. exact None.
-Defined.
+Definition f_proj (i : [T_intermediate]) : forall n, [Option (my_f_I n)] :=
+  fun n => match n with
+           | 4 => i     (*the high process receives the disk-interrupt handler's output*)
+           | _ => None  (*every other process receives nothing*)
+           end.
 
+(* my_process_pool =
+     process_pool 5 my_f_coopt my_f_initial my_f_I my_f_O T_intermediate f_proj my_f_pid my_procs *)
 Definition my_process_pool := @process_pool cur_pid 5 my_f_coopt my_f_initial my_f_I my_f_O T_intermediate f_proj my_f_pid my_procs.
 
 
-
-
-
-
-
-(* 2) Stateful wrapper*)
-
+(* === 3. Stateful wrapper === *)
 
 Definition inr_or_def {A B : Set} (def: B) (x : A + B) := if x is inr x' then x' else def.
 (*We define a stateful wrapper that will be wrapped around the process pool*)
+(* loop_sta state f_I def p f_si =
+     map inl (inr_or_def def)
+       (loop (map id snd
+         (sta f_I (fun _ v => v) state
+           (map f_si inr (maybe p))))) *)
 Definition loop_sta
   (cur_pid stateType : Ty)           
   (state : [stateType])
@@ -238,9 +260,7 @@ Definition loop_sta
                                       (Option (Times cur_pid T')) _ (Sum T_in T_out) f_si inr (maybe p)))))).
 
 
-
-
-(* 3) Model state*)
+(* === 4. Model state === *)
 Definition mask := Bool.
 Definition pending := Bool.
 Definition I_bits := Times pending mask.
@@ -328,7 +348,7 @@ Definition masks_set (v : [stateType]) :=
   foldr (fun I b => (get_I_mask v I) && b) true all_interrupts.
 
 
-(* 4) State transitions*)
+(* === 5. State transitions === *)
 Definition step_left (f : [T_in] -> [stateType] -> [stateType]) : [Sum T_in T_out'] -> [stateType] -> [stateType] :=
   fun i v => 
   match i with
@@ -412,8 +432,8 @@ Definition state_step (handler_preroutine : [T_out'] -> [stateType] -> [stateTyp
 (*we wrap initiate_next in step_right even though it does not use the input to ensure we only apply this step on output updates, this is important for the last case of f_EP for initiate_next*)
 
 
-(*helper definitions for defining traces for the models*)
-(*model1 trace*)
+(* === 6. Trace vocabulary === *)
+(* Named inputs and outputs used to write the example traces for each model. *)
 Definition Tsum' := ([T_in] + [T_out'])%type.
 Definition Tsum := Sum T_in T_out. 
 
@@ -455,7 +475,7 @@ Definition seqtype := seq ([T_in] + [T_out]).
 
 
 
-(*Model1*)
+(* === 7. model_bad === *)
 
 Definition false_I_bits : [I_bits] := (false,false).
 Definition false_ic : [ic] := ((false,false),(false_I_bits,false_I_bits)).
@@ -492,10 +512,12 @@ Definition def : [ T_out' ]  := (None,(None,(None,(None,(None,None))))).
 Definition f_si (si : [Times stateType (Sum T_in T_out')]) : [Option (Times cur_pid T_intermediate)] :=
   if si.2 is inr o then Some (get_cur_pid si.1, dI_out o) else None.
 
+(* model_bad = loop_sta initial_state (state_step bad_preroutine id) def my_process_pool f_si *)
 Definition model_bad : Proc T_in T_out' := @loop_sta cur_pid stateType initial_state T_in T_out' T_intermediate (state_step bad_preroutine id) def my_process_pool f_si.
 
 
 
+(* model_bad traces *)
 Definition no_dI' : seqtype' :=   [::pub_get';                                     tI';pub_get';tI_no';tI_yes';sch_high;pr_nop'(*nop*);tI';pr_nop';tI_no';tI_yes';sch_low;pub_get'].
 Definition with_dI' : seqtype' := [::pub_get';dI';pub_get';dI_no';dI_yes';pub_get';tI';pub_get';tI_no';tI_yes';sch_high;pr_sys'(*sys*);tI';pr_nop';tI_no';tI_yes';sch_low;pub_get'].
 
@@ -550,7 +572,7 @@ Qed.
 
 
 
-(*model2*)
+(* === 8. model_good === *)
 Definition mask_most : [ic] := ((false,true),((false,true),(false,false))). (*mask set for everything but timer interrupt*)
 
 Definition init_ir_count : [ir_count] := Some 4.
@@ -601,9 +623,10 @@ Definition bool_coding v := let b := timeslice_live (get_ir_count v) in
 
 Definition good_preroutine (o : [T_out']) : [stateType] -> [stateType] := check_ir_count \o check_handler_completed \o (initiate_ir o). (*\o (unset_handler_masks o)*) 
 
+(* model_good = loop_sta initial_state_good (state_step good_preroutine bool_coding) def my_process_pool f_si *)
 Definition model_good := @loop_sta cur_pid stateType initial_state_good T_in T_out' T_intermediate (state_step good_preroutine bool_coding) def my_process_pool f_si.
 
-(*traces for model2*)
+(* model_good traces *)
 Definition good_no_dI' : seqtype' :=   [::pub_get';                      tI';pub_get';tI_no';tI_yes';defaultI_no';defaultI_yes';defaultI_no';defaultI_yes';sch_high;pr_nop'(*nop*);tI';pr_nop';tI_no';tI_yes';defaultI_no';defaultI_yes';defaultI_no';defaultI_yes';sch_low;pub_get'].
 Definition good_with_dI' : seqtype' := [::pub_get';dI';pub_get';pub_get';tI';pub_get';tI_no';tI_yes';dI_no'      ;dI_yes'      ;defaultI_no';defaultI_yes';sch_high;pr_sys'(*sys*);tI';pr_nop';tI_no';tI_yes';defaultI_no';defaultI_yes';defaultI_no';defaultI_yes';sch_low;pub_get'].
 
@@ -643,7 +666,7 @@ Qed.
 
 
 
-(*model3*)
+(* === 9. wrapped_model_good === *)
 Definition parse_output (o : [T_out']) : [T_out] :=
   match o with
   | (Some public,_) => Some (inl public)
@@ -654,7 +677,7 @@ Definition parse_output (o : [T_out']) : [T_out] :=
 
 Definition wrapped_model_good : Proc T_in T_out := map id parse_output model_good.
 
-(*traces for model3*)
+(* wrapped_model_good traces *)
 Definition final_out_rel : myrel [T_out] := eqmaybe_false (eqsum (publicRel _) (semiprivateRel _)).
 
 
